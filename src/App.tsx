4af84d0a29b70c736ead77afae93cbdc241b043a
ace34TT <tafinasoa35@gmail.com>
2023-03-23 13:56:31 +0300

    create:homepages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import Frontoffice from "./views/layouts/frontoffice/Main/Frontoffice";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Frontoffice />}></Route>
+      <Route path="/" element={<Frontoffice />}>
+        <Route path="/" element={<Homepage />} />
+      </Route>
       {/* <Route path="applicant" element={<Template />}>
         <Route path="application-form" element={<RegistrationForm />}></Route>
         <Route
