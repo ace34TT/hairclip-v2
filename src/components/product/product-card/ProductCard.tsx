ed61a6779d02ea9fdc6bf2b6f9b65ed33e7b3a7e
ace34TT <tafinasoa35@gmail.com>
2023-03-29 02:41:13 +0300

    style : lazy loading colors images

--- src/components/product/product-card/ProductCard.tsx
+++ src/components/product/product-card/ProductCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import imgPlaceHolder from "./../../../assets/place-holder/b&w.jpg";
 import { useNavigate } from "react-router-dom";
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 interface IProps {
   id: string;
   name: string;
@@ -21,17 +23,25 @@ export default function ProductCard({
     <div className="inline-block prose max-w-none">
       <div
         style={{ backgroundColor: color, minWidth: "384px" }}
-        className="flex w-80 md:w-96 py-6 flex-col items-center rounded-3xl prose hover:scale-105 hover:shadow-md transition ease-in-out cursor-pointer break-words"
+        className="flex w-80 md:w-96 py-6 flex-col gap-4 items-center rounded-3xl prose hover:scale-105 hover:shadow-md transition ease-in-out cursor-pointer break-words"
       >
-        <h2 className="text-white">{name}</h2>
-        <img src={preview} className="rounded-3xl" alt="" />
+        <h2 className="text-white m-0">{name}</h2>
+        <LazyLoadImage
+          src={preview}
+          placeholderSrc={imgPlaceHolder}
+          alt="Image Alt"
+          width={300}
+          height={300}
+          className="rounded-3xl m-0"
+          effect="blur"
+        />
+        {/* <img src={preview} className="rounded-3xl" alt="" /> */}
         <div className="text-slate-50 w-10/12 text-center  inline-block">
           <div className="flex justify-center ">
-            <div className="text-2xl">{price} €</div>
+            <div className="text-2xl"> {price} €</div>
           </div>
           <div className="">et 5€ à partir de 3 chouchous acheté</div>
         </div>
-        <br />
         <button
           onClick={() => {
             navigate("/produits/" + id);
